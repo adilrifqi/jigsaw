56788a8f062072fa5699b40e804639c46bf53053
Rifqi Adlan Apriyadi <apriyadi.r.aa@m.titech.ac.jp>
2022-09-20 22:23:52 +0900

    Refactor to use a map for storing variables

--- webviews/jigsawView/FlowComponent.tsx
+++ webviews/jigsawView/FlowComponent.tsx
@@ -2,6 +2,7 @@ import { useCallback, useState } from "react";
 import React = require("react");
 import ReactFlow, { addEdge, Background, Connection, Controls, Edge, MiniMap, Node, useEdgesState, useNodesState } from "react-flow-renderer";
 import { DebugState } from "./DebugState";
+import { JigsawVariable } from "./JigsawVariable";
 
 export function FlowComponent() {
     // Hooks
@@ -15,20 +16,30 @@ export function FlowComponent() {
     // Listen for DAP messages sent from the extension
     window.addEventListener('message', event => {
         let data = event.data
-        if (data["command"] == "variables") {
-            DebugState.getInstance().variables = data["body"]["variables"];
+
+        // parse and update the DebugState if the message is a "variables" response
+        if (data["type"] == "response" && data["command"] == "variables") {
+            for (var variable of data["body"]["variables"]) {
+                const parsedVariable: JigsawVariable | undefined = parseVariable(variable);
+                if (parsedVariable) {
+                    DebugState.getInstance().updateVariable(parsedVariable);
+                }
+            }
+            console.log(DebugState.getInstance().jigsawVariables);
         }
 
-        const varNodes = []
-        for (var variable of DebugState.getInstance().variables) {
-            const varName = variable["name"];
+        // Compile the variable nodes and update the view
+        const varNodes: React.SetStateAction<Node<any>[]>
+            | { id: string; type: string; data: { label: string; }; position: { x: number; y: number; }; }[] = [];
+        DebugState.getInstance().jigsawVariables.forEach((variable: JigsawVariable, key: string) => {
+            const varName = variable.name;
             varNodes.push({
                 id: varName,
                 type:'input',
-                data: {label: varName + ": " + variable["value"]},
+                data: {label: varName + ": " + variable.value},
                 position: { x: 250, y: 25 }
             });
-        }
+        });
         setNodes(varNodes);
     })
 
@@ -47,4 +58,37 @@ export function FlowComponent() {
             <Background/>
           </ReactFlow>
     );
+}
+
+function parseVariable(toParse: {[key: string]: any}): JigsawVariable | undefined {
+    const name: string = toParse["name"];
+    const value: string = toParse["value"];
+    const type: string = toParse["type"];
+    const variablesReference: number = toParse["variablesReference"];
+    const namedVariables: number = toParse["namedVariables"];
+    const indexedVariables: number = toParse["indexedVariables"];
+    const evaluateName: string = toParse["evaluateName"];
+
+    // console.log("=================================");
+    // console.log("name: " + name);
+    // console.log("value: " + value);
+    // console.log("type: " + type);
+    // console.log("variablesReference: " + variablesReference);
+    // console.log("namedVariables: " + namedVariables);
+    // console.log("indexedVariables: " + indexedVariables);
+    // console.log("evaluateName: " + evaluateName);
+    
+    if (
+        !name
+        || !value
+        || !type
+        || variablesReference == undefined
+        || namedVariables == undefined
+        || indexedVariables == undefined
+        || !evaluateName
+        ) {
+            console.log("undedededede");
+        return undefined;
+    }
+    return new JigsawVariable(name, value, type, variablesReference, namedVariables, indexedVariables, evaluateName);
 }
