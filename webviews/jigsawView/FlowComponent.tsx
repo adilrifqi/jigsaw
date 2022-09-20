bf8cc72b93b2894d9ea051760a28b86552e1872b
Rifqi Adlan Apriyadi <apriyadi.r.aa@m.titech.ac.jp>
2022-09-20 11:40:36 +0900

    Convert FlowComponent into a function component

--- webviews/jigsawView/FlowComponent.tsx
+++ webviews/jigsawView/FlowComponent.tsx
@@ -1,42 +1,38 @@
-import * as React from "react";
-import ReactFlow, { addEdge, Background, Connection, Controls, Edge, MiniMap, useEdgesState, useNodesState } from "react-flow-renderer";
+import { useState } from "react";
+import React = require("react");
+import ReactFlow, { Background, Controls, MiniMap } from "react-flow-renderer";
 import { DebugState } from "./DebugState";
 
-export class FlowComponent extends React.Component<{}, { variables: { [key: string]: string }[] }> {
-    private _dState: DebugState = DebugState.getInstance();
+export function FlowComponent() {
+    const [variables, setVariables] = useState(() => DebugState.getInstance().variables);
 
-    constructor(props: {} | Readonly<{}>) {
-        super(props);
-        this.state = {
-            variables: this._dState.variables
+    // Listen for DAP messages sent from the extension
+    window.addEventListener('message', event => {
+        let data = event.data
+        if (data["command"] == "variables") {
+            DebugState.getInstance().variables = data["body"]["variables"];
+            setVariables(_ => DebugState.getInstance().variables);
         }
+    })
+
+    const varNodes = []
+    for (var variable of variables) {
+        const varName = variable["name"];
+        varNodes.push({
+            id: varName,
+            type:'input',
+            data: {label: varName + ": " + variable["value"]},
+            position: { x: 250, y: 25 }
+        });
     }
 
-    render(): React.ReactNode {
-        window.addEventListener('message', event => {
-            let data = event.data
-            if (data["command"] == "variables") {
-                this._dState.variables = data["body"]["variables"];
-                this.setState({variables: this._dState.variables});
-            }
-        })
-
-        const varNodes = []
-        for (var variable of this.state.variables) {
-            const varName = variable["name"];
-            varNodes.push({id: varName, type:'input', data: {label: varName + ": " + variable["value"]}, position: { x: 250, y: 25 }});
-        }
-
-        // return <h1>Hello</h1>;
-
-        return (
-            <ReactFlow
-              nodes={varNodes}
-              fitView>
-                <MiniMap/>
-                <Controls/>
-                <Background/>
-              </ReactFlow>
-        );
-    }
+    return (
+        <ReactFlow
+          nodes={varNodes}
+          fitView>
+            <MiniMap/>
+            <Controls/>
+            <Background/>
+          </ReactFlow>
+    );
 }
